467bfd3f7fd152e0226ced219e2fb70f244cac06
IrinaWilson <keysii@yandex.ru>
2024-04-02 12:40:15 +1100

    integrated js for a form and answer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function showAnswer(event) {
+  event.preventDefault();
 
+  let factsAnswerElement = document.querySelector("#facts-answer");
+  new Typewriter("#facts-answer", {
+    strings:
+      "A pistol shrimp can make a sound louder than a jet engine by snapping its claw",
+    autoStart: true,
+    cursor: null,
+    delay: 4,
+  });
+}
+
+let formElement = document.querySelector("#facts-generator");
+formElement.addEventListener("submit", showAnswer);
