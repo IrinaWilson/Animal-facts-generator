3284cd597aa24b940f580803030e28344d173529
IrinaWilson <keysii@yandex.ru>
2024-04-02 17:57:24 +1100

    Integrated API

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-function showAnswer(event) {
-  event.preventDefault();
-
+function displayFact(response) {
   let factsAnswerElement = document.querySelector("#facts-answer");
   new Typewriter("#facts-answer", {
-    strings:
-      "A pistol shrimp can make a sound louder than a jet engine by snapping its claw",
+    strings: response.data.answer,
     autoStart: true,
     cursor: null,
     delay: 4,
   });
+}
+
+function showAnswer(event) {
+  event.preventDefault();
+  let instructionsInput = document.querySelector("#user-instruction");
+  let context =
+    "You are an animal expert and know a lot of interesting facts about them, please write short and unique facts about animals. You mission is generate an interesting fact about animals. Make sure the answer is short. Do not include a title. Make sure to follow the user instructions.";
+  let prompt = `User instructions: Generate an interesting fact about ${instructionsInput.value}`;
+
+  let apiKey = "80083fa950bb6bac505970t4eao412fe";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayFact);
 }
 
 let formElement = document.querySelector("#facts-generator");
