c4f63b6531bffa5afd4b2638dcfc568eeabf8675
IrinaWilson <keysii@yandex.ru>
2024-04-05 11:32:12 +1100

    edited context and a prompt

--- src/index.js
+++ src/index.js
@@ -16,8 +16,8 @@ function showAnswer(event) {
   event.preventDefault();
   let instructionsInput = document.querySelector("#user-instruction");
   let context =
-    "You are an animal expert and know a lot of interesting facts about them, please write short and unique facts about animals. You mission is generate an interesting fact about animals. Make sure the answer is short. Do not include a title. Make sure to follow the user instructions.";
-  let prompt = `User instructions: Generate an interesting fact about ${instructionsInput.value}`;
+    "You are an animal expert and know a lot of interesting facts about them, please write three short and unique facts about animals. You mission is to generate a 3 line answer in basic HTML and separate each line with a <br />, do not repeat user instructions on each line. Do not include a title. Make sure to follow the user instructions.";
+  let prompt = `User instructions: Generate interesting facts about ${instructionsInput.value}`;
 
   let apiKey = "80083fa950bb6bac505970t4eao412fe";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
@@ -25,7 +25,7 @@ function showAnswer(event) {
   let hiddenElement = document.querySelector("#facts-answer");
   hiddenElement.classList.remove("hidden");
 
-  hiddenElement.innerHTML = `<span class = "generating">⏳</span> Generating a fact about ${instructionsInput.value}`;
+  hiddenElement.innerHTML = `<span class = "generating">⏳</span> Generating facts about ${instructionsInput.value}`;
   clearInput();
   axios.get(apiUrl).then(displayFact);
 }
